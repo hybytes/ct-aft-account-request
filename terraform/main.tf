191d2eb304aa25e7e662206beb2ae2ffbd85a7a5
wahab-hussain-hybytes <wahab.hussain@hybytes.com>
2025-04-18 18:14:53 +0500

    aft code update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,64 @@
-module "environment_account" {
-  for_each = { for env in var.environments : env.name => env }
-    source = "./modules/aft-account-request"
-    control_tower_parameters = {
-      AccountEmail             = "${each.value.company}+${each.value.name}@${each.value.domain}"
-      AccountName              = "${each.value.name} account"
-      ManagedOrganizationalUnit = each.value.environment_managed_ou
-      SSOUserEmail             = "${each.value.company}+${each.value.name}@${each.value.domain}"
-      SSOUserFirstName         = each.value.F_user
-      SSOUserLastName          = each.value.L_user
-    }
+company_name = "hybytes"
+domain = "hybytes.com"
+ct_home_region = "eu-west-1"
+tf_backend_secondary_region = "eu-west-1"
 
-    account_tags = {
-      "${each.value.company}:Owner"       = "${each.value.company}+${each.value.name}@${each.value.domain}"
-      "${each.value.company}:Environment" = each.value.name
-      "${each.value.company}:Vended"      = "true"
-    }
 
-    change_management_parameters = {
-      change_requested_by = "${each.value.company} ${each.value.name}"
-      change_reason       = "Request account for ${each.value.name} environment"
-    }
+# Account Request for "prod account"
+module "prod account" {
+  source = "../modules/aft-account-request"
+  control_tower_parameters = 
+  {
+    AccountName = "prod account"
+    AccountEmail = "hybytes+prod@hybytes.com"
+    ManagedOrganizationalUnit = "prodOU"
+    SSOUserEmail = "prod@hybytes.com"
+    SSOUserFirstName = "prod"
+    SSOUserLastName = "hybytes"
+  }
+#Account Tags here
+  account_tags = 
+  {
+    "hybytes:Owner" = "prod@hybytes.com"
+    "hybytes:Environment" = "prod"
+    "hybytes:Vended" = "true"
+  }
 
-    account_customizations_name = "${each.value.name}-customizations"
+
+  change_management_parameters = 
+  {
+    change_requested_by = "hybytes prod"
+    change_reason       = "Request account for prod environment"
+  }
+
+account_customizations_name  = "prod-customizations"
+}# Account Request for "dev account"
+module "dev account" {
+  source = "../modules/aft-account-request"
+  control_tower_parameters = 
+  {
+    AccountName = "dev account"
+    AccountEmail = "hybytes+dev@hybytes.com"
+    ManagedOrganizationalUnit = "devOU"
+    SSOUserEmail = "dev@hybytes.com"
+    SSOUserFirstName = "dev"
+    SSOUserLastName = "hybytes"
+  }
+#Account Tags here
+  account_tags = 
+  {
+    "hybytes:Owner" = "dev@hybytes.com"
+    "hybytes:Environment" = "dev"
+    "hybytes:Vended" = "true"
+  }
+
+
+  change_management_parameters = 
+  {
+    change_requested_by = "hybytes dev"
+    change_reason       = "Request account for dev environment"
+  }
+
+account_customizations_name  = "dev-customizations"
 }
+
